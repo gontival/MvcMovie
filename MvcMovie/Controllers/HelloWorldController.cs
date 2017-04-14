c1f524b5b2bc1550efd157ccd7201313d24d30ae
MCNEXT\jlevant <jlevant@mcnext.com>
2017-04-14 16:55:23 +0200

    Todo Adding a View : https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-a-view

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        /// <summary>
+        /// GET : /HelloWorld/
+        /// </summary>
+        /// <returns></returns>
+        public string Index()
+        {
+            return "This is my <b>default<b/> action...";
+        }
+
+        public string Welcome(string name, int id =1)
+        {
+            return HttpUtility.HtmlEncode($"Hello {name}, ID: {id}");
+        }
+        
+
+    }
+}
